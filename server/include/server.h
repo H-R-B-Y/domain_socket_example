2a6c5bc3a468f2dfea711d1a400ddeb4fff3b6b7
H-R-B-Y <hbreeze@student.42.fr>
2025-10-08 17:28:26 +0100

    Added the run file for the server, probably a lot of bugs to work out, also not checking the error messages right now, will have to go through them later. made a start on a very simple implementation

--- server/include/server.h
+++ server/include/server.h
@@ -5,100 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbreeze <hbreeze@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/03 14:37:46 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/10/06 14:22:29 by hbreeze          ###   ########.fr       */
+/*   Created: 2025/10/08 16:31:30 by hbreeze           #+#    #+#             */
+/*   Updated: 2025/10/08 16:32:54 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
-# ifndef _POSIX_C_SOURCE
-#  define _POSIX_C_SOURCE 1
-# endif
-# ifndef _POSIX_SOURCE
-#  define _POSIX_SOURCE
-# endif
-# ifndef _XOPEN_SOURCE
-#  define _XOPEN_SOURCE 700
-# endif
-
-
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include <errno.h>
-# include <string.h>
-# include <signal.h>
-# include <sys/socket.h>
-# include <sys/epoll.h>
-# include <sys/stat.h>
-# include <sys/un.h>
+# include "shared_resource.h"
+# include "socket_framework.h"
 
 # define FT_INCLUDE_ALL
 # include "libft.h"
 
-# include "shared_resource.h"
-
-# ifndef SOCKET_PATH
-#  define SOCKET_PATH "/tmp/server_5790.sock"
-# endif
+# include <unistd.h>
 
 
-
-
-
-struct s_connection
-{
-	int	fd;
-	int	epoll_parent; // The fd associated with the reader of this fd
-
-	enum e_partial_state
-	{
-		PARTIAL_STATE_PRECHUNK,
-		PARTIAL_STATE_HEADER,
-		PARTIAL_STATE_CONTENT,
-	}	read_state;
-	size_t	bytes_read;
-	size_t	bytes_expected;
-	int		prechunk;
-	struct s_header_chunk header;
-	char	*content_buffer;
-	int		msg_complete;
-};
-
-struct s_room
-{
-
-};
-
-typedef int (*t_msg_handler)(void *srv, struct s_header_chunk *header, int client);
-
-
-struct s_server
-{
-	int					server_fd;
-	int					epoll_fd;
-	struct sockaddr_un	addr;
-	t_cdll				*connections;
-	t_cdll				*rooms;
-	t_msg_handler		handlers[MTYPE_TYPE_COUNT];
-};
-
-
-void	main_loop(struct s_server *srv);
-void	terminate_child(void *chld);
-void	terminate_room(void *room);
-void terminate_srv(struct s_server *srv, const char *msg, t_u8 ret_code);
-int init_server(struct s_server *srv);
-void	setup_signal_handler(void);
-
-void	create_new_connection(struct s_server *srv);
-int	handle_msg(struct s_server *srv, int fd);
-int	handle_disconnect(struct s_server *srv, int fd);
-int	remove_connection_for_fd(struct s_server *srv, int fd);
-int	handle_message(struct s_server *srv, struct s_connection *conn);
-
-void	broadcast_message(struct s_server *srv, char *buffer, size_t size);
 
 #endif
--- server/src/main.c
+++ server/src/main.c
@@ -5,29 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbreeze <hbreeze@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/03 15:02:56 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/10/04 11:01:15 by hbreeze          ###   ########.fr       */
+/*   Created: 2025/10/08 16:32:59 by hbreeze           #+#    #+#             */
+/*   Updated: 2025/10/08 17:26:23 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "server.h"
+#include "server.h"
+
+// typedef int		(*t_on_connect_fn)(struct s_server *srv, struct s_connection *conn, void *appdata);
+// typedef int		(*t_on_message_fn)(struct s_server *srv, struct s_message *message, void *appdata);
+// typedef void	(*t_on_disconnect_fn)(struct s_server *srv, struct s_connection *conn, void *appdata);
+
+int		on_connect(struct s_server *srv, struct s_connection *conn, void *appdata)
+{
+	(void)srv;
+	(void)appdata;
+	// We have already created the app data so lets just print something
+	printf("Client connected with fd %d and id %zu\n", conn->fd, conn->id);
+	return (1);
+}
+
+int	on_message(struct s_server *srv, struct s_message *msg, void *appdata)
+{
+	(void)srv;
+	(void)appdata;
+	printf("Recieved message from client with fd %d and id %zu\n", msg->sender->fd, msg->sender->id);
+	printf("Message content: %s", msg->content);
+	return (1);
+}
+
+void	on_disconnect(struct s_server *srv, struct s_connection *conn, void *appdata)
+{
+	(void)srv;
+	(void)appdata;
+	printf("Client with fd %d and id %zu has disconnected\n", conn->fd, conn->id);
+	return ;
+}
+
 
 int main(void)
 {
-	struct s_server	srv = {0};
-	// first we want to ensure that we are a singleton
-	setup_signal_handler();
-	if (socket_exists())
-	{
-		dprintf(STDERR_FILENO, "Socket file %s already exists\nTerminating\n", SOCKET_PATH);
-		return (1);
-	}
-	if (!init_server(&srv))
-	{
-		dprintf(STDERR_FILENO, "Terminating\n");
-		return (1);
-	}
-	main_loop(&srv);
-	terminate_srv(&srv, "Ok!", 0);
+	struct s_server	srv;
+
+	printf("connecting to socket %s\n", SOCKET_PATH);
+	if (server_init(&srv, SOCKET_PATH) < 0)
+		return (0);
+	srv.appdata = 0;
+	srv.free_appdata = 0;
+	srv.on_connect = on_connect;
+	srv.on_message = on_message;
+	srv.on_disconnect = on_disconnect;
+	server_run(&srv);
+	server_shutdown(&srv);
 	return (0);
 }
